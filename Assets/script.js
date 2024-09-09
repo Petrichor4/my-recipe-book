bdbb25d1ea303284a1b21d7904c6ecbe844d3434
Kierstyn Hart <stonepreston05@gmail.com>
2024-09-09 17:45:38 -0600

    testing card layout and partially implemented card flipping

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,19 @@
 const addNew = document.getElementById('add-new');
+const card = document.getElementById('card');
 
 // Function for adding a new recipe card to the page
-function createElement() {
+function createCard() {
     const div = document.createElement('div');
     const img = document.createElement('img');
     const h2 = document.createElement('h2');
     const p = document.createElement('p');
     const a = document.createElement('a');
-    const section = document.getElementById('recipes');
-
-    section.appendChild('div');
-    
+    const section = document.getElementById('recipes');    
 }
+card.addEventListener('click', function() {
+    if (card.classList.contains('flipped')) {
+        card.classList.remove('flipped');
+    } else {
+        card.classList.add('flipped');
+    }
+})

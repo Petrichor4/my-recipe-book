6c0b5062e33f915cce2a3d901ba3cc923aad88df
Preston Stone <stonepreston05@gmail.com>
2024-09-09 14:57:49 -0600

    Saving progress

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+const addNew = document.getElementById('add-new');
+
+// Function for adding a new recipe card to the page
+function createElement() {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    const a = document.createElement('a');
+    const section = document.getElementById('recipes');
+
+    section.appendChild('div');
+    
+}

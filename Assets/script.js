85caf53e43707db9175c75998dd4e7fb749f9b9e
Preston Stone <stonepreston05@gmail.com>
2024-09-15 20:30:25 -0600

    Added side pop out for adjective filter

--- Assets/script.js
+++ Assets/script.js
@@ -73,10 +73,11 @@ document.getElementById('save-recipe').addEventListener('click', function() {
   const description = document.getElementById('recipe-description').value;
 
   const newRecipe = {
+      id: generateRandomNumber(1, 500),
       title: title,
       imgUrl: imgUrl,
       linkUrl: linkUrl,
-      description: description
+      description: description,
   };
 
   // Save to localStorage
@@ -95,43 +96,45 @@ document.getElementById('save-recipe').addEventListener('click', function() {
 function createRecipeCard(recipe) {
   const newRecipeCard = document.createElement('article');
   newRecipeCard.classList.add('my-5', 'card');
+  newRecipeCard.setAttribute('data-id', recipe.id); // Add data-id attribute
   newRecipeCard.innerHTML = `
       <div id="front" class="flex justify-start">
         <img src="${recipe.imgUrl}" alt="" />
+        <i class=" p-3 fa-solid fa-x fa-2xs" style="color: #dbdfe6;" onclick="deleteCard(${recipe.id})"></i> <!-- Add onclick handler -->
         <div id="info" class="flex items-start flex-col">
           <h2 class="w-full block px-4 py-2 text-3xl text-center">
             ${recipe.title}
           </h2>
           <p class="p-4">${recipe.description}</p>
         </div>
-        <div class="flip-icon" onclick="flipCard(this)"><i class="fa-solid fa-repeat fa-rotate-by fa-sm" style="--fa-rotate-angle: 90deg;""></i>
+        <div class="flip-icon" onclick="flipCard(this)"><i class="fa-solid fa-repeat fa-rotate-by fa-sm" style="--fa-rotate-angle: 90deg;"></i>
         </div>
       </div>
       <div id="back">
         <h2>Recipe:</h2>
-        <p id="recipe" contenteditable="true" class="w-full h-4/5"${recipe.userRecipe}></p>
+        <p id="recipe" contenteditable="true" class="w-full h-4/5"></p>
         <a href="${recipe.linkUrl}" target="_blank">Go to Recipe</a>
         <div class="flip-icon" onclick="flipCard(this)">
-        <i class="fa-solid fa-repeat fa-rotate-by fa-sm" style="--fa-rotate-angle: 90deg;""></i>
+        <i class="fa-solid fa-repeat fa-rotate-by fa-sm" style="--fa-rotate-angle: 90deg;"></i>
         </div>
       </div>
   `;
-
   // Insert the new recipe card before the "Add New Recipe" card
   const addNewCard = document.getElementById('new-card');
   addNewCard.parentNode.insertBefore(newRecipeCard, addNewCard);
+  console.log(recipe)
 }
-
 function saveRecipeToLocalStorage(recipe) {
   let recipes = JSON.parse(localStorage.getItem('recipes')) || [];
   recipes.push(recipe);
   localStorage.setItem('recipes', JSON.stringify(recipes));
+  
 }
 function openMenu() {
     const menu = document.querySelector('aside');
+    menu.classList.toggle('hidden');
     menu.classList.toggle('open');
 }
-
 
 function loadSavedRecipes() {
   const recipes = JSON.parse(localStorage.getItem('recipes')) || [];
@@ -141,4 +144,31 @@ function loadSavedRecipes() {
 function flipCard(element) {
   const card = element.closest('.card');
   card.classList.toggle('flipped');
+}
+
+function login() {
+  window.location.href = 'login.html'
+}
+
+function deleteCard(recipeId) {
+  // Retrieve the list of recipes from localStorage
+  let recipes = JSON.parse(localStorage.getItem('recipes')) || [];
+  // Find the index of the recipe with the given recipeId
+  const recipeIndex = recipes.findIndex(recipe => recipe.id === recipeId);
+  // If the recipe is found, remove it from the list
+  if (recipeIndex !== -1) {
+    recipes.splice(recipeIndex, 1);
+    // Save the updated list back to localStorage
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+    // Find the card element in the DOM using the recipeId
+    const cardElement = document.querySelector(`.card[data-id="${recipeId}"]`);
+    // Remove the card element from the DOM
+    if (cardElement) {
+      cardElement.remove();
+    }
+  }
+}
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
